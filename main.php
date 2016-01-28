d1438fa5627e11a107c5ddd9ed4f4b0a72c650af
VipulNarayana <mnvipul@gmail.com>
2016-01-28 20:15:29 +0530

    Create main.php

---
+++ main.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+</head>
+<body>
+<!--Creating a form to allow the user to give a name -->
+<form method="GET" action="test.php">
+<!-- Creating a text box -->
+Name:<input type="text" name="user" placeholder="Enter your name" required>
+
+<input type="submit" >
+</form>
+
+</body>
